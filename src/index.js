b1ac5e4524b84b3e2bd77f7969dc030f6d5ff1f4
Cinthia Rodriguez <599034+cinthiaro@users.noreply.github.com>
2025-02-22 15:59:24 -0600

    Configurar dotenv y supabase.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+//Crear servidor con express
 const express = require('express'); 
 const app = express(); 
 const PORT = 3005; 
-app.listen(PORT, () => { 
+
+app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`); 
 });
+
+
+//Cargar variables de entorno
+require('dotenv').config();
+
+const { createClient } = require('@supabase/supabase-js');
+//Establecer conexión con Supabase
+const supabase = createClient( process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY );
+
+//APIs
 
 app.get('/', (req, res) => {
     res.send('Mi Proyecto con Express');
@@ -13,6 +25,12 @@ app.get('/hola', (req, res) => {
     res.send('Hola Mundo');
 });
 
-app.get('/api', (req, res) => {
-    res.json({mensaje: "Bienvenido al API"});
-});
+app.get('/tareas', (req, res) => {
+  supabase
+  .from('tareas')
+  .select('*')
+  .then(({ data, error }) => {
+    if (error) console.error(error);
+    else res.send(data);
+  });
+})
